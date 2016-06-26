4aea284f019097ef9f088a6812c034507d7a8c84
Tyler Scott <tyduptyler13@gmail.com>
2016-06-26 04:50:38 +0000

    Fixed attack function, almost done.

--- spies.cpp
+++ spies.cpp
@@ -7,16 +7,30 @@ using namespace std;
 
 #define N 8
 #define tc 0
+#define maxMoves 25
 
 typedef vector<vector<unsigned> > Board;
 
 volatile bool solved = false; //For stopping other threads early.
 
+//Board access: [row][column]
+
+inline unsigned gcd( int x, int y ){
+    if( x < y ) swap( x, y );
+
+    while( y > 0 ){
+        const int f = x % y;
+        x = y;
+        y = f;
+    }
+    return x;
+}  
+
 void attack(Board& board, const vector<unsigned>& pos){
     
     for (unsigned row = 0; row < N; ++row){
         
-        unsigned col = pos[row];
+        const unsigned col = pos[row];
         
         //Block out cols
         for (unsigned i = 0; i < N; ++i){
@@ -26,15 +40,35 @@ void attack(Board& board, const vector<unsigned>& pos){
         }
         
         
-        //FIXME Need to check this logic for the loops. Too bored to do so.
         //Block left horizontals
-        for(unsigned r = ){
-            board[row + (col - x)][x]++;
+        if (col <= row){
+            for (unsigned y = row - col, x = 0; y < N; ++x, ++y){ //y will grow to N faster so skip checking x
+                if (y == row) continue; //Skip ourself, we can't attack ourself
+                board[y][x]++;
+            }
+        } else {
+            for (unsigned y = 0; x = col - row; x < N; ++x, ++y){
+                if (y == row) continue; //Skip ourself, we can't attack ourself
+                board[y][x]++;
+            }
         }
+        
         //Block right horizontals
-        for(int x = col; x < N && row - (col - x) < N; ++x){
-            board[row - (col - x)][x]++;
+        const unsigned iy = N - row - 1;
+        if (iy <= col){
+            for (unsigned y = N - 1, x = col - iy; x < N; --y, ++x){
+                if (y == row) continue;
+                board[y][x]++;
+            }
+        } else {
+            for (unsigned y = row - iy, x = 0; y >= 0; --y; ++x){
+                if (y == row) continue;
+                board[y][x]++;
+            }
         }
+        
+        //TODO mark all integer intersections of spies lined up
+        
         
     }
     
@@ -42,26 +76,54 @@ void attack(Board& board, const vector<unsigned>& pos){
 
 void solve(Board board){
     
-    //Set initial positions randomly
-    vector<unsigned> pos(N);
-    random_device rand;
-    uniform_int_distribution<unsigned> dist(0,N);
-    for (unsigned& r : pos){
-        r = dist(rand);
-    }
-    
-    //Set the attack levels on the board.
-    attack(board, pos);
-    
-    for (auto& r : board){
-        
-        for (unsigned i : r){
-            
-            cout << i;
-            
+    while (!solved){
+        //Set initial positions randomly
+        vector<unsigned> pos(N);
+        random_device rand;
+        uniform_int_distribution<unsigned> dist(0,N);
+        for (unsigned& r : pos){
+            r = dist(rand);
         }
         
-        cout << endl;
+        const int moves = maxMoves;
+        while (maxMoves > 0 && !solved){
+        
+            //Set the attack levels on the board.
+            attack(board, pos);
+
+            vector<unsigned> conflicted;
+            for (unsigned i = 0; i < N; ++i){
+                if (board[i][pos[i]] > 0){
+                    conflicted.push_back(i);
+                }
+            }
+
+            if (conflicted == 0 && !solved){ //Win and no other thread finished first.
+                
+                solved = true;
+            
+                for (auto& r : board){
+                    
+                    for (unsigned i : r){
+                        
+                        cout << i;
+                        
+                    }
+                    
+                    cout << endl;
+                    
+                }
+            
+            } else {
+                
+                //Randomly choose a queen to move to its best position and continue.
+                
+                
+            }
+        
+        }
+        
+        
         
     }
     
